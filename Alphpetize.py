f14c97031ffd846c2e50b50002162bce5ada7d0e
Sander van Dragt <sander.vandragt@gmail.com>
2014-02-25 16:54:20 +0000

    Windows compatibility

--- Alphpetize.py
+++ Alphpetize.py
@@ -2,7 +2,7 @@ import sublime, sublime_plugin, re
 
 class AlphpetizeCommand(sublime_plugin.TextCommand):
 
-	newline_styles = {'unix': '\n', 'lf': '\n', 'windows': '\r\n', 'crlf': '\r\n', 'mac os 9': '\r', 'cr': '\r'}
+	newline_styles = {'unix': '\n', 'lf': '\n', 'windows': '\n', 'crlf': '\r\n', 'mac os 9': '\r', 'cr': '\r'}
 
 	def run(self, edit):
 		"""
